66a0699617331caf2bfedeef90968f02dead7536
rlatndus0 <ksy36057@naver.com>
2024-08-30 02:00:52 +0900

    Update ai음성.js

--- headers/ai음성.js
+++ headers/ai음성.js
@@ -1,7 +1,6 @@
+function (region, temp, high, low, description, windDirection, windSpeed, pm10, pm2.5, overallGrade){
  const XI_API_KEY = 'sk_0d53f3cc7f143353f57b79ad670f424957015c2cf2a1c73f'; // api 
   const url = 'https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM'; //목소리 불러오는 api 및 목소리 설정
-
-function (region, temp, high, low, description, windDirection, windSpeed, pm10, pm2.5, overallGrade){
   const data = {
       "text": " 현재 위치는 ${region}이며, 현재 온도는 ${temp}도입니다. 오늘 최고 온도는 ${high}도 이며, 최저 온도는 ${low}도 입니다. 오늘 날씨 상태는 ${description}입니다. 풍향은 ${windDirection}도이며, 풍속은 ${windSpeed}m/s입니다. 미세먼지 농도는 ${pm10}이며, 초미세먼지 농도는 ${pm2.5}입니다. 공기질 상태는 ${overallGrade}입니다.",
       "model_id": "eleven_multilingual_v2",
